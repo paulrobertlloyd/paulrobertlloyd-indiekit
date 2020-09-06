0c3f6ffd52f975ce52239445f1b5ba0c02be47ce
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-09-06 13:36:52 +0100

    feat: upgrade to latest version of Indiekit

--- index.js
+++ index.js
@@ -1,104 +1,104 @@
 import 'dotenv/config.js';
 import {Indiekit} from '@indiekit/indiekit';
+import {JekyllPreset} from '@indiekit/preset-jekyll';
 import {GithubStore} from '@indiekit/store-github';
 
 // New indiekit instance
 const indiekit = new Indiekit();
 
+// Configire publication preset
+const jekyll = new JekyllPreset();
+
 // Configure content store
-const githubStore = new GithubStore({
+const github = new GithubStore({
   user: 'paulrobertlloyd',
   repo: 'paulrobertlloyd-v4',
   branch: 'main',
   token: process.env.GITHUB_TOKEN
 });
 
-const publicationConfig = {
-  categories: {
-    url: 'https://paulrobertlloyd.com/categories/index.json'
+const postTypes = [{
+  type: 'article',
+  name: 'Article',
+  post: {
+    path: 'src/content/articles/{yyyy}-{MM}-{dd}-{slug}.md',
+    url: '{yyyy}/{MM}/{slug}/'
   },
-  'post-types': [{
-    type: 'article',
-    name: 'Article',
-    post: {
-      path: 'src/content/articles/{yyyy}-{MM}-{dd}-{slug}.md',
-      url: '{yyyy}/{MM}/{slug}/'
-    },
-    media: {
-      path: 'src/images/{yyyy}/{MM}/{filename}',
-      url: 'images/{yyyy}/{MM}/{filename}'
-    }
-  }, {
-    type: 'note',
-    name: 'Note',
-    post: {
-      path: 'src/content/notes/{t}.md',
-      url: 'notes/{t}/'
-    }
-  }, {
-    type: 'photo',
-    name: 'Photo',
-    post: {
-      path: 'src/content/photos/{t}.md',
-      url: 'photos/{t}/'
-    },
-    media: {
-      path: 'src/images/photos/{t}.{ext}',
-      url: 'images/photos/{t}.{ext}'
-    }
-  }, {
-    type: 'bookmark',
-    name: 'Bookmark',
-    post: {
-      path: 'src/content/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md',
-      url: 'bookmarks/{yyyy}/{MM}/{slug}/'
-    }
-  }, {
-    type: 'event',
-    name: 'Event',
-    post: {
-      path: 'src/content/events/{yyyy}-{MM}-{dd}-{slug}.md',
-      url: 'events/{yyyy}/{MM}/{slug}/'
-    }
-  }, {
-    type: 'rsvp',
-    name: 'Reply with RSVP',
-    post: {
-      path: 'src/content/replies/{t}.md',
-      url: 'replies/{t}/'
-    }
-  }, {
-    type: 'reply',
-    name: 'Reply',
-    post: {
-      path: 'src/content/replies/{t}.md',
-      url: 'replies/{t}/'
-    }
-  }],
-  'slug-separator': '_',
-  'syndicate-to': [{
-    uid: 'https://twitter.com/paulrobertlloyd/',
-    name: '@paulrobertlloyd on Twitter'
-  }, {
-    uid: 'https://mastodon.social/@paulrobertlloyd',
-    name: '@paulrobertlloyd on Mastodon'
-  }, {
-    uid: 'https://micro.blog/paulrobertlloyd',
-    name: '@paulrobertlloyd on Micro.blog'
-  }]
-}
+  media: {
+    path: 'src/images/{yyyy}/{MM}/{filename}',
+    url: 'images/{yyyy}/{MM}/{filename}'
+  }
+}, {
+  type: 'note',
+  name: 'Note',
+  post: {
+    path: 'src/content/notes/{t}.md',
+    url: 'notes/{t}/'
+  }
+}, {
+  type: 'photo',
+  name: 'Photo',
+  post: {
+    path: 'src/content/photos/{t}.md',
+    url: 'photos/{t}/'
+  },
+  media: {
+    path: 'src/images/photos/{t}.{ext}',
+    url: 'images/photos/{t}.{ext}'
+  }
+}, {
+  type: 'bookmark',
+  name: 'Bookmark',
+  post: {
+    path: 'src/content/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md',
+    url: 'bookmarks/{yyyy}/{MM}/{slug}/'
+  }
+}, {
+  type: 'event',
+  name: 'Event',
+  post: {
+    path: 'src/content/events/{yyyy}-{MM}-{dd}-{slug}.md',
+    url: 'events/{yyyy}/{MM}/{slug}/'
+  }
+}, {
+  type: 'rsvp',
+  name: 'Reply with RSVP',
+  post: {
+    path: 'src/content/replies/{t}.md',
+    url: 'replies/{t}/'
+  }
+}, {
+  type: 'reply',
+  name: 'Reply',
+  post: {
+    path: 'src/content/replies/{t}.md',
+    url: 'replies/{t}/'
+  }
+}];
 
-// Register extensions
-indiekit.addStore(githubStore);
+const syndicationTargets =  [{
+  uid: 'https://twitter.com/paulrobertlloyd/',
+  name: '@paulrobertlloyd on Twitter'
+}, {
+  uid: 'https://mastodon.social/@paulrobertlloyd',
+  name: '@paulrobertlloyd on Mastodon'
+}, {
+  uid: 'https://micro.blog/paulrobertlloyd',
+  name: '@paulrobertlloyd on Micro.blog'
+}];
 
 // Application settings
+indiekit.set('application.mongodbUrl', process.env.MONGODB_URL)
 indiekit.set('application.locale', 'en-GB');
 
 // Publication settings
-indiekit.set('publication.config', publicationConfig);
+indiekit.set('publication.categories', 'https://paulrobertlloyd.com/categories/index.json');
 indiekit.set('publication.me', 'https://paulrobertlloyd.com');
+indiekit.set('publication.postTypes', postTypes);
+indiekit.set('publication.preset', jekyll);
 indiekit.set('publication.slugSeparator', '_');
-indiekit.set('publication.storeId', 'github');
+indiekit.set('publication.store', github);
+indiekit.set('publication.syndicationTargets', syndicationTargets);
 
 // Server
 const server = indiekit.server();
