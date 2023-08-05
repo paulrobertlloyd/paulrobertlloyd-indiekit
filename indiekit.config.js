46ec982099d85aaf2b35d976ae1e66471b749a12
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-05 01:11:25 +0100

    feat: update config

--- indiekit.config.js
+++ indiekit.config.js
@@ -8,65 +8,71 @@ export default {
     "@indiekit/endpoint-json-feed",
     "@indiekit/preset-jekyll",
     "@indiekit/store-github",
+    "@indiekit/syndicator-internet-archive",
     "@indiekit/syndicator-mastodon",
   ],
   publication: {
     categories: "https://paulrobertlloyd.com/categories/index.json",
     locale: "en-GB",
     me: "https://paulrobertlloyd.com",
-    postTypes: [{
-      type: "article",
-      name: "Article",
-      post: {
-        path: "articles/{yyyy}-{MM}-{dd}-{slug}.markdown",
-        url: "{yyyy}/{DDD}/a{n}/{slug}/"
+    postTypes: [
+      {
+        type: "article",
+        name: "Article",
+        post: {
+          path: "articles/{yyyy}-{MM}-{dd}-{slug}.markdown",
+          url: "{yyyy}/{DDD}/a{n}/{slug}/",
+        },
+        media: {
+          path: "media/{yyyy}/{DDD}/a{n}/{filename}",
+          url: "{yyyy}/{DDD}/a{n}/{filename}",
+        },
       },
-      media: {
-        path: "media/{yyyy}/{DDD}/a{n}/{filename}",
-        url: "{yyyy}/{DDD}/a{n}/{filename}"
-      }
-    }, {
-      type: "note",
-      name: "Note",
-      post: {
-        path: "notes/{yyyy}-{MM}-{dd}-{n}.markdown",
-        url: "{yyyy}/{DDD}/n{n}/"
-      }
-    }, {
-      type: "photo",
-      name: "Photo",
-      post: {
-        path: "photos/{yyyy}-{MM}-{dd}-{n}.markdown",
-        url: "{yyyy}/{DDD}/p{n}/"
+      {
+        type: "note",
+        name: "Note",
+        post: {
+          path: "notes/{yyyy}-{MM}-{dd}-{n}.markdown",
+          url: "{yyyy}/{DDD}/n{n}/",
+        },
       },
-      media: {
-        path: "media/{yyyy}/{DDD}/p{n}/{filename}",
-        url: "{yyyy}/{DDD}/p{n}/{filename}"
-      }
-    }, {
-      type: "bookmark",
-      name: "Bookmark",
-      post: {
-        path: "bookmarks/{yyyy}-{MM}-{dd}-{slug}.markdown",
-        url: "{yyyy}/{DDD}/b{n}/{slug}/"
-      }
-    }, {
-      type: "reply",
-      name: "Reply",
-      post: {
-        path: "replies/{yyyy}-{MM}-{dd}-{n}.markdown",
-        url: "{yyyy}/{DDD}/r{n}/"
-      }
-    }],
+      {
+        type: "photo",
+        name: "Photo",
+        post: {
+          path: "photos/{yyyy}-{MM}-{dd}-{n}.markdown",
+          url: "{yyyy}/{DDD}/p{n}/",
+        },
+        media: {
+          path: "media/{yyyy}/{DDD}/p{n}/{filename}",
+          url: "{yyyy}/{DDD}/p{n}/{filename}",
+        },
+      },
+      {
+        type: "bookmark",
+        name: "Bookmark",
+        post: {
+          path: "bookmarks/{yyyy}-{MM}-{dd}-{slug}.markdown",
+          url: "{yyyy}/{DDD}/b{n}/{slug}/",
+        },
+      },
+      {
+        type: "reply",
+        name: "Reply",
+        post: {
+          path: "replies/{yyyy}-{MM}-{dd}-{n}.markdown",
+          url: "{yyyy}/{DDD}/r{n}/",
+        },
+      },
+    ],
     slugSeparator: "_",
   },
   "@indiekit/store-github": {
     user: "paulrobertlloyd",
-    repo: "paulrobertlloyd-content"
+    repo: "paulrobertlloyd-content",
   },
   "@indiekit/syndicator-mastodon": {
     checked: true,
-    forced: false,
     url: "https://mastodon.social",
     user: "paulrobertlloyd",
   },
